a4a0b3c1c44ebc7f50147214d28d61a9c6e6b74d
0xMounir <izaku6666@gmail.com>
2025-01-28 18:52:46 +0100

    add jokes to db and updating files

--- app.js
+++ app.js
@@ -27,7 +27,45 @@ app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
-sequelize.sync().then(() => console.log("Synchronized database"));
+sequelize.sync().then(async () => {
+  console.log("Synchronized database");
+
+  const blagues = [
+    { content: "Quelle est la femelle du hamster ? - L’Amsterdam" },
+    { content: "Que dit un oignon quand il se cogne ? - Aïe" },
+    {
+      content:
+        "Quel est l'animal le plus heureux ? - Le hibou, parce que sa femme est chouette.",
+    },
+    {
+      content: "Pourquoi le football c'est rigolo ? - Parce que Thierry en rit",
+    },
+    {
+      content:
+        "Quel est le sport le plus fruité ? - La boxe, parce que tu te prends des pêches dans la poire et tu tombes dans les pommes.",
+    },
+    { content: "Que se fait un Schtroumpf quand il tombe ? - Un Bleu" },
+    { content: "Quel est le comble pour un marin ? - Avoir le nez qui coule" },
+    {
+      content:
+        "Qu'est ce que les enfants usent le plus à l'école ? - Le professeur",
+    },
+    { content: "Quel est le sport le plus silencieux ? - Le para-chuuuut" },
+    {
+      content:
+        "Quel est le comble pour un joueur de bowling ? - C’est de perdre la boule",
+    },
+  ];
+
+  await Promise.all(
+    blagues.map(async (blague) => {
+      const newJoke = await Joke.create(blague);
+      console.log(`Added joke : ${newJoke.content}`);
+    })
+  );
+
+  console.log("Jokes added to db");
+});
 
 app.post("/v1/blagues", async (req, res) => {
   try {
@@ -49,22 +87,23 @@ app.get("/v1/blagues", async (req, res) => {
   }
 });
 
-app.get("/v1/blagues/:id", async (req, res) => {
+app.get("/v1/blagues/random", async (req, res) => {
   try {
-    const joke = await Joke.findByPk(req.params.id);
-    if (!joke) {
-      return res.status(404).json({ error: "Joke not found" });
-    }
-    res.json(joke);
+    const jokes = await Joke.findAll();
+    const randomJoke = jokes[Math.floor(Math.random() * jokes.length)];
+    res.json(randomJoke);
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: "Internal Server Error" });
   }
 });
 
-app.get("/v1/blagues/random", async (req, res) => {
+app.get("/v1/blagues/:id", async (req, res) => {
   try {
-    const joke = await Joke.findOne({ order: [sequelize.fn("RANDOM")] });
+    const joke = await Joke.findByPk(req.params.id);
+    if (!joke) {
+      return res.status(404).json({ error: "Joke not found" });
+    }
     res.json(joke);
   } catch (error) {
     console.error(error);
